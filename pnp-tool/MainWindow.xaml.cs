9aa66d6c8318064c9f0c3d0cb2add1ab5872b29f
Chr1zM <ch.me010103@gmail.com>
2023-07-17 18:13:19 +0200

    [WIP] Add Navigation Bar / Tab Bar#6 * new TabControl with TabItems * Button for new Tabs * New Tabs don't work yet

--- pnp-tool/MainWindow.xaml.cs
+++ pnp-tool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pnp_tool.CharacterSheet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,25 @@ namespace pnp_tool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Creates a new tab item with a character sheet page and adds it to the tab control.
+        /// <br/> Optionally, selects the newly added tab to display it.
+        /// </summary>
+        /// <param name="sender">The object that triggered the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void AddNewTabButton_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem newTabItem = new TabItem();
+            // TODO: Give the Tab (CharacterSheet) a Name https://github.com/Chr1zM/pnp-tool/issues/11
+            newTabItem.Header = "New Character Sheet";
+            newTabItem.Content = new CharacterSheetPage();
+
+            CharacterSheetTabs.Items.Add(newTabItem);
+
+            // Navigate to the created Tab
+            //CharacterSheetTabs.SelectedItem = newTabItem;
         }
     }
 }
